f7ae5570fb055d6b2e170c9270ab4a2f33dd9a58
Subhajit Sahu <wolfram77@gmail.com>
2022-07-28 22:21:45 +0530

    :mango: process output logs

--- process.js
+++ process.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const os = require('os');
+const path = require('path');
 
 const RGRAPH = /^Loading graph .*\/(.*?)\.mtx \.\.\./m;
-const RORDER = /^order: (\d+) size: (\d+) \{\}/m;
-const RRESLT = /^\[(.+?) ms; (\d+) iters\.\] \[(.+?) err\.\] (\w+)/m;
+const RORDER = /^order: (\d+) size: (\d+) (?:\[\w+\] )?\{\} \(selfLoopAllVertices\)/m;
+const RORGNL = /^\[(\S+?) modularity\] noop/;
+const RRESLT = /^\[(\S+?) ms; (\d+) iterations; (\d+) passes; (\S+?) modularity\] louvainSeq(?: \{acc_capacity=(\d+)\})?/m;
 
 
 
@@ -52,17 +54,25 @@ function readLogLine(ln, data, state) {
     state.order = parseFloat(order);
     state.size  = parseFloat(size);
   }
+  else if (RORGNL.test(ln)) {
+    var [, modularity] = RORGNL.exec(ln);
+    data.get(state.graph).push(Object.assign({}, state, {
+      time:                 0,
+      iterations:           0,
+      passes:               0,
+      modularity:           parseFloat(modularity),
+      accumulator_capacity: 0,
+    }));
+  }
   else if (RRESLT.test(ln)) {
-    var [, time, iterations, error, technique] = RRESLT.exec(ln);
-    data.get(state.graph).push({
-      graph: state.graph,
-      order: state.order,
-      size:  state.size,
-      time:       parseFloat(time),
-      iterations: parseFloat(iterations),
-      error:      parseFloat(error),
-      technique:  technique,
-    });
+    var [, time, iterations, passes, modularity, accumulator_capacity] = RRESLT.exec(ln);
+    data.get(state.graph).push(Object.assign({}, state, {
+      time:                 parseFloat(time),
+      iterations:           parseFloat(iterations),
+      passes:               parseFloat(passes),
+      modularity:           parseFloat(modularity),
+      accumulator_capacity: parseFloat(accumulator_capacity || '0'),
+    }));
   }
   return state;
 }
@@ -99,10 +109,15 @@ function processCsv(data) {
 
 function main(cmd, log, out) {
   var data = readLog(log);
+  if (path.extname(out)==='') cmd += '-dir';
   switch (cmd) {
     case 'csv':
       var rows = processCsv(data);
       writeCsv(out, rows);
+      break;
+    case 'csv-dir':
+      for (var [graph, rows] of data)
+        writeCsv(path.join(out, graph+'.csv'), rows);
       break;
     default:
       console.error(`error: "${cmd}"?`);
